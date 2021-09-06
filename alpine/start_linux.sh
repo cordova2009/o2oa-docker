242963474d5ed6f6b4237ec71979176a325a101d
cordova2009 <yujunyi@power28.com.cn>
2021-09-06 11:32:13 +0800

    init code

---
+++ alpine/start_linux.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Copyright (c) http://www.o2oa.net/
+current_dir="$(cd "$(dirname "$0")"; pwd)"
+java_home="/opt/java/openjdk"
+if [ -d ${current_dir}/local/update ]
+then
+	for D in commons configSample localSample jvm servers store config  local
+	do
+		if [ ! -d ${current_dir}/$D ]
+		then
+			mkdir ${current_dir}/commons
+		fi
+	done
+	if [ -f ${current_dir}/local/update/o2server/version.o2 ]
+	then
+		echo 'update o2server.'
+		for D in commons configSample localSample jvm servers store
+		do
+			if [ -d ${current_dir}/local/update/o2server/$D ]
+			then
+				echo "update ${current_dir}/$D."
+				cp -Rf -p ${current_dir}/local/update/o2server/$D  ${current_dir}/
+			fi
+		done
+		for F in console.jar index.html src.zip
+		do
+			if [ -f ${current_dir}/local/update/o2server/$F ]
+			then
+				echo "update ${current_dir}/$F."
+				cp -f -p ${current_dir}/local/update/o2server/$F ${current_dir}/
+			fi
+		done
+		for A in "start" "stop" "restart" "console" "service"
+		do
+			for B in "_windows.bat" "_linux.sh" "_macos.sh"
+			do
+				if [ -f ${current_dir}/local/update/o2server/$A$B ]; then
+					echo "update ${current_dir}/$A$B."
+					cp -f -p ${current_dir}/local/update/o2server/$A$B ${current_dir}/
+				fi
+			done
+		done
+		echo "update ${current_dir}/version.o2."
+		cp ${current_dir}/local/update/o2server/version.o2 ${current_dir}/
+		echo "clean local/update directory."
+		rm -Rf ${current_dir}/local/update
+		echo "the update is complete, please restart the server."
+		exit 1
+	fi
+fi
+setsid ${java_home}/bin/java -Dnashorn.args=--no-deprecation-warning --add-exports jdk.scripting.nashorn/jdk.nashorn.internal.runtime=ALL-UNNAMED --add-exports jdk.scripting.nashorn/jdk.nashorn.internal.runtime.arrays=ALL-UNNAMED -javaagent:${current_dir}/console.jar -server -Djava.awt.headless=true -Xms2g -Duser.timezone=GMT+08 -XX:+HeapDumpOnOutOfMemoryError -jar ${current_dir}/console.jar
